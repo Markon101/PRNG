4d4758c9d10ed8a0106ba66c4fd903f69f65ce0e
Daniel <daniel@danieljosephjones.com>
2021-11-14 10:50:09 -0500

    Updated right shift and added loop for calc

--- PRNG.py
+++ PRNG.py
@@ -12,20 +12,22 @@ while True:
     # Get frame
     if vidc.isOpened():
         returnvalue, frame = vidc.read()
-    pixel = frame[300][300][1]
-    pixel_2 = frame[55][20][1]
-    pixel_3 = frame[400][400][1]
     # Get clock
     clk = time.clock_gettime(1)
-    for i in range(20):
-        if (pixel < cv2.INTER_MAX):
-            pixel *= 2 + pixel_2 + pixel_3
-        clk += clk+pixel
+    # Do x interations of the algorithm
+    for i in range(30):
+        # Add the value of 400 pixels to the clk variable
+        for x in range(400):
+            for y in range (400):
+                clk += frame[x][y][1]*2
         clk += seed
-        clk = np.right_shift(int(clk), 2)
+        clk = int(clk) >> 2
         clk += int(clk) % 3
         clk *= int(clk) % 2 + 1
-        clk += clk % i
+        if(i != 0):
+            clk += clk % i
+        clk += clk % 2
+        clk *= 4
         if(clk not in testlist):
             print(clk)
         # Test list for debugging PRNG
